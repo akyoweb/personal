c9aa4b140b35e76476cabd722e5743cce1bcbc0c
akyo <mmdj8004@gmail.com>
2026-09-24 22:51:08 +0330

    Delete assets/files/proje directory

--- assets/files/proje/aio/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$db = mysqli_connect('localhost','root','','gps');
-date_default_timezone_set("Asia/Tehran");
-mysqli_set_charset($db, 'utf8');
-
-?>
--- assets/files/proje/aio/images/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$db = mysqli_connect('aiolearnfamily.com','aiolear3_admingps','gps1402@!','aiolear3_gps');
-date_default_timezone_set("Asia/Tehran");
-mysqli_set_charset($db, 'utf8');
-
-?>
